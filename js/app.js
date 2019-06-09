b6209901e75f14001082c3ad9d54aed27685cf8f
gitk47320 <34836412+gitk47320@users.noreply.github.com>
2019-06-09 21:17:27 +0900

    　都道府県を検索キーとして追加 　都道府県を検索キーとして追加

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ function gnaviFreewordSearch(offset, hit_per_page){
   var req = new XMLHttpRequest();
   keyid = '71926b2013dd9d0d36651401e85520a4'
   var freeword = document.forms.mainform.elements['freeword'].value
-  var apiresult = null
-  url = `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=71926b2013dd9d0d36651401e85520a4&freeword=${freeword}&offset=${offset}&hit_per_page=${hit_per_page}`
+  var pref = document.forms.mainform.elements['pref'].value
+  url = `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=71926b2013dd9d0d36651401e85520a4&pref=${pref}&freeword=${freeword}&offset=${offset}&hit_per_page=${hit_per_page}`
   
   req.responseType = 'json'
   
@@ -11,6 +11,8 @@ function gnaviFreewordSearch(offset, hit_per_page){
 
   req.onload = function(){
     for(i = 0; i < hit_per_page; i++){
+    //APIで取得した店の名前と画像を表示
+    //それぞれをぐるなびのサイトにリンクしている
     document.getElementById("shops").childNodes[i].innerHTML 
     = `<a href = ${req.response.rest[i].url}><img src = ${req.response.rest[i].image_url.shop_image1}><br>${req.response.rest[i].name}</a><br>`
     }
